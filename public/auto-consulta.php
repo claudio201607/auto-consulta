d41259dcb15f5e267eaa1683f835fe930b9d5116
CLAUDIO_CORTEZ <claudioernestocortezastorga07@gmail.com>
2020-04-04 12:05:35 -0300

    update 1

---
+++ public/auto-consulta.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="img/bootstrap-solid.svg">
+  <title>Bootstrap  4 examples</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery-3.2.1.slim.min.js"></script>
+<script src="vendor/jquery/popper.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+</html>
